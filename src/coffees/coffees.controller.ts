c38f01566dbd29b57a1612a97e55ccdb44e1b1cd
cec <gules01@gmail.com>
2021-07-03 19:41:37 +0200

    Update coffees.controller.ts

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -7,8 +7,49 @@ export class CoffeesController {
     return 'return all coffees';
   }
 
+  @Get(':id')
+  findOne(@Param() params) {
+    return `return one coffee by id: ${params.id}`;
+  }
+
   @Get('flavors')
   findAllFlavors() {
     return 'return all flavors';
+  }
+
+  @Get('flavors/:id')
+  findOneFlavor(@Param('id') id: string) {
+    return `return one flavor by id: ${id}`;
+  }
+
+  @Get(':coffeeId/flavors/:flavorId/:color')
+  findFlavorForCoffee(@Param() params) {
+    console.log('v1');
+    return `coffee: ${params.coffeeId} - flavor: ${params.flavorId} - Color: ${params.color}`;
+  }
+
+  // This will never be executed because first is matched above route
+  // Is shown here as an example
+  @Get(':coffeeId/flavors/:flavorId/:color')
+  findFlavorForCoffeeV2(
+    @Param('coffeeId') coffeeId: string,
+    @Param('flavorId') flavorId: string,
+    @Param('color') color: string,
+  ) {
+    console.log('v1');
+    return `coffee: ${coffeeId} - flavor: ${flavorId} - Color: ${color}`;
+  }
+
+  @Post()
+  createCoffee(@Body() body) {
+    console.log(body);
+    return body;
+  }
+
+  @Post(':coffeeId/flavors/:flavorId')
+  createFlavorColor(@Body('name') body) {
+    // body only contains the value of filed 'name'
+    console.log(body);
+    return body;
   }
 }
