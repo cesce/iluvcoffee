cb50e2d30e82e3d345f39ab17379d420b92eedb7
cec <gules01@gmail.com>
2021-07-11 21:33:23 +0200

    new coffee service

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { Coffee } from './entities/coffee.entity';
 
 @Injectable()
 export class CoffeesService {
   // We will use the array as datasource, mocking the real database
-  private coffees = [];
+  private coffee: Coffee[] = [];
 }
