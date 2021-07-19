d26c12f859a5c4d3726599c1f242b5a7f4e3d84c
cec <gules01@gmail.com>
2021-07-19 10:06:45 +0200

    update dto

--- src/coffees/dto/create-coffee.dto.ts
+++ src/coffees/dto/create-coffee.dto.ts
@@ -1,5 +1,12 @@
+import { IsString } from 'class-validator';
+
 export class CreateCoffeeDto {
+  // @IsString decorator makes the property required
+  @IsString()
   readonly name: string;
+
+  @IsString()
   readonly brand: string;
-  readonly flavors: string[];
+
+  readonly flavors?: string[];
 }
